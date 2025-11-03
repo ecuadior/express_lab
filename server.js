b07b2861aa35609a6baddb44239c81d9ad4a9c2d
ecuadior <john.guam.9000@gmail.com>
2025-11-03 13:40:49 -0500

    added simple request and ejs

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express(); //Calling this function set up server
+app.set('view engine', 'ejs');
 
 app.get('/',(req,res)=>
     {
         console.log('Here');
-        res.send("Hello World!");
-    });// this function woill run whne someone go to the root folder
+        res.render("index");
+        //res.status(500).send("Hellow World");
+        //res.download('server.js'); start donwload when user refresh
+    });// this function will run whne someone go to the root folder
 
-app.listen(3030);
+app.listen(3030);//port im running at 
 
