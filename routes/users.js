7a27eb7da39420701b33722fd8e7a2d12871103e
ecuadior <john.guam.9000@gmail.com>
2025-11-12 14:18:41 -0500

    added stuff from class

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,9 @@ router.get('/new',(req, res)=>
 {
     res.send('New User Form');
 }); 
-
+router.post('/',(req,res)=>{
+    
+});
 //router.get('/:id',(req,res)=>{
 //   res.send(`Getting User Data: ${req.params.id}`);
 //});
@@ -21,6 +23,11 @@ router.route("/:id").get((req,res)=>{
     res.send(`Deleting user with id: ${req.params.id}`);
 }).put((req,res)=>{
     res.send(`Updating user with id: ${req.params.id}`);
+});
+const users = [{name:"George"},{name:"Yess"}];
+router.param("id",(req, res, next, id)=>{
+    console.log(`Accesing user #${id}`);
+    next();
 });
 
 module.exports =router;
